405e60658ce2dfbbc2841b24b637fb3450a8275c
davidwilson-nid <davidwilsonphd@gmail.com>
2023-03-03 13:35:13 -0700

    in class code from 2023-03-02

--- 9 feature engineering/Section 1.R
+++ 9 feature engineering/Section 1.R
@@ -42,7 +42,7 @@ df %>%
 df <- df %>% 
   mutate(age = if_else(is.na(age), mean(age, na.rm = T), age))
  
-
+mean(df$age)
 # now handle embarked, this time using replace_na()
 # Again, check our work
 df <- df %>% 
@@ -69,22 +69,42 @@ df %>%
   summary()
 
 # calculate extreme threshold caps based on 99th percentile
-age_cap <- quantile(df$age, .99)
+age_cap <-    quantile(df$age, .99)
 sib_sp_cap <- quantile(df$sib_sp, .99)
-parch_cap <- quantile(df$parch, .99)
-fare_cap <- quantile(df$fare, .99)
+parch_cap <-  quantile(df$parch, .99)
+fare_cap <-   quantile(df$fare, .99)
 
 # Optional: Create a tibble for easy comparison:
+tibble(column = outlier_candidates,
+       caps = c(age_cap, sib_sp_cap, parch_cap, fare_cap))
 
 # Now check how many are beyond the percentile caps
+df %>% 
+  summarize(count_over_age = sum(age > age_cap),
+            count_over_sib_sp = sum(sib_sp > sib_sp_cap),
+            count_over_parch = sum(parch > parch_cap),
+            count_over_fare = sum(fare > fare_cap))
 
 
 
 # cap age and fare, and check work before saving
+df <- df %>% 
+  mutate(fare = if_else(fare > fare_cap, fare_cap, fare)) 
 
 
 # save the result to df
 
+
+
+df %>% 
+  ggplot(aes(x = age)) +
+  geom_histogram() +
+  theme_bw() 
+
+df %>% 
+  ggplot(aes(x = fare)) +
+  geom_histogram() +
+  theme_bw() 
 
 
 
@@ -126,11 +146,41 @@ df %>%
     fare_th  = fare^(1/2),
     fare_t1  = fare^1,
     fare_t2  = fare^2
-  ) 
+  ) %>% 
+  select(starts_with('fare_')) %>% 
+  pivot_longer(starts_with('fare')) %>% 
+  ggplot(aes(x = value, fill = name)) +
+  geom_density(alpha = .4) +
+  facet_wrap(~name, scales = 'free', ncol = 1)
 
 # now let's visualize the effect of the transformations to see 
 # which one makes sense.
 
 
+# Note here that we need to add one to the fare before applying the power transformation. Zero raised to the
+# power of -1/2 is not a number. Sometimes, to make a transformation work, we have to protect the transformation
+# from the possible math errors. In this case, a horizontal shift of 1 does not affect the distribution and
+# the column is eventually going to be standardized anyway. 
+# (This is what caused the errors we saw in class...)
+df <- df %>% 
+  mutate(fare = (fare+1)^-(1/2))
 
+
+# Now we can scale the numeric columns using the old-school `scale()` function. Because it's an old function,
+# it returns the data in a fairly annoying format (as a one-column matrix), so we'll have to cast the result
+# as a numeric vector so that it can be treated as a column in the mutate function:
+df %>% 
+  select(where(is.numeric)) %>% 
+  mutate(across(c(age,sib_sp,parch,fare), ~as.numeric(scale(.x))))
+
+# Alternatively, we could write our own scaling function and use it in the mutate, which would look something
+# like this:
+scale_this <- function(x){
+  (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+}
+
+# Then we could use the function as we normally would inside the mutate:
+df %>% 
+  select(where(is.numeric)) %>% 
+  mutate(across(c(age,sib_sp,parch,fare), ~scale_this(.x)))
 
--- 9 feature engineering/Section 2.R
+++ 9 feature engineering/Section 2.R
@@ -64,23 +64,45 @@ df %>%
 outlier_candidates <- c('age', 'sib_sp', 'parch', 'fare')
 
 # Pass the four columns to summary() to check means, maxes
-
+df %>% 
+  select(all_of(outlier_candidates)) %>% 
+  summary()
 
 # calculate extreme threshold caps based on 99th percentile
+age_cap <- quantile(df$age, .99)
+fare_cap <- quantile(df$fare, .99)
+sib_sp_cap <- quantile(df$sib_sp, .99)
+parch_cap <- quantile(df$parch, .99)
 
+c(age_cap, fare_cap, sib_sp_cap, parch_cap)
 
 # Optional: Create a tibble for easy comparison:
 
+df %>% select(age)
 # Now check how many are beyond the percentile caps
+df %>% 
+  summarize(count_past_age = sum(age > age_cap),
+            count_past_fare = sum(fare>fare_cap),
+            count_past_sib_sp = sum(sib_sp>sib_sp_cap),
+            count_past_parch = sum(parch>parch_cap))
+
 
 
 # cap age and fare, and check work before saving
+df <- df %>% 
+  mutate(age = if_else(age > age_cap, age_cap, age),
+         fare = if_else(fare > fare_cap, fare_cap, fare)) 
 
 
 # save the result to df
 
 
-
+df %>% 
+  ggplot(aes(x = age)) +
+  geom_histogram()
+df %>% 
+  ggplot(aes(x = fare)) +
+  geom_histogram()
 
 # Transforming Features ---------------------------------------------------------------------------------
 # Here's the basic idea behind Box-Cox transformations:
@@ -99,6 +121,9 @@ tribble(
 
 
 # Examine distributions of age and fare
+
+
+
 
 
 # Let's transform the fare column
@@ -120,11 +145,45 @@ df %>%
     fare_th  = fare^(1/2),
     fare_t1  = fare^1,
     fare_t2  = fare^2
-  ) 
+  ) %>% 
+  select(starts_with('fare_')) %>% 
+  pivot_longer(everything()) %>% 
+  ggplot(aes(x = value, fill = name)) +
+  geom_density(alpha = .5) +
+  facet_wrap(~name, scales = 'free', ncol = 1 )
+
+
+
+
 
 # now let's visualize the effect of the transformations to see 
 # which one makes sense.
 
+# Note here that we need to add one to the fare before applying the power transformation. Zero raised to the
+# power of -1/2 is not a number. Sometimes, to make a transformation work, we have to protect the transformation
+# from the possible math errors. In this case, a horizontal shift of 1 does not affect the distribution and
+# the column is eventually going to be standardized anyway. 
+# (This is what caused the errors we saw in class...)
+df <- df %>% 
+  mutate(fare = (fare+1)^-(1/2))
 
+
+# Now we can scale the numeric columns using the old-school `scale()` function. Because it's an old function,
+# it returns the data in a fairly annoying format (as a one-column matrix), so we'll have to cast the result
+# as a numeric vector so that it can be treated as a column in the mutate function:
+df %>% 
+  select(where(is.numeric)) %>% 
+  mutate(across(c(age,sib_sp,parch,fare), ~as.numeric(scale(.x))))
+
+# Alternatively, we could write our own scaling function and use it in the mutate, which would look something
+# like this:
+scale_this <- function(x){
+  (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+}
+
+# Then we could use the function as we normally would inside the mutate:
+df %>% 
+  select(where(is.numeric)) %>% 
+  mutate(across(c(age,sib_sp,parch,fare), ~scale_this(.x)))
 
 
